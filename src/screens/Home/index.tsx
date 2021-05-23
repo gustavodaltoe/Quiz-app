d23aba744fd0b4db20287c6032e3a77e2ff8c6f1
gustavodaltoe <gustavodaltoe@hotmail.com>
2021-05-23 18:48:49 -0300

    logout button

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,13 +1,14 @@
 import React, { useEffect } from 'react';
 import { useNavigation } from '@react-navigation/native';
 import { BackHandler } from 'react-native';
+import { MaterialCommunityIcons as Icon } from '@expo/vector-icons';
 
 import logo from '../../assets/logo.png';
 import { MainButtons } from '../../components/MainButtons';
 import { GradientBackground } from '../../components/GradientBackground';
 import { Spacer } from '../../components/Spacer';
 
-import { Wrapper, LogoImg } from './styles';
+import { Wrapper, LogoImg, LogoutButton, LogoutText } from './styles';
 
 export function Home() {
   const navigation = useNavigation();
@@ -23,6 +24,10 @@ export function Home() {
     });
   }, [navigation]);
 
+  const handleLogout = () => {
+    navigation.navigate('sign-in');
+  };
+
   return (
     <GradientBackground>
       <Wrapper>
@@ -31,6 +36,12 @@ export function Home() {
         <Spacer flex={1} />
 
         <MainButtons />
+
+        <LogoutButton onPress={handleLogout}>
+          <LogoutText>
+            <Icon name="logout" color="#fff" size={16} /> Log out
+          </LogoutText>
+        </LogoutButton>
       </Wrapper>
     </GradientBackground>
   );
--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -1,3 +1,4 @@
+import { RectButton } from 'react-native-gesture-handler';
 import styled from 'styled-components/native';
 
 export const Wrapper = styled.SafeAreaView`
@@ -12,4 +13,19 @@ export const LogoImg = styled.Image.attrs({
 })`
   width: 250px;
   height: 250px;
+`;
+
+export const LogoutButton = styled(RectButton)`
+  justify-content: center;
+  align-items: center;
+  height: 42px;
+  width: 290px;
+  margin-top: 14px;
+  background: transparent;
+`;
+
+export const LogoutText = styled.Text`
+  font-size: 16px;
+  color: #fff;
+  font-family: 'Roboto_500Medium';
 `;
